04a204c887b397c1559c4dd650e393134a56826c
Dileep Ranganathan <dileep.ranganathan@intel.com>
2018-07-23 07:16:25 -0700

    Fix Docker image to use the nexus artifacts
    Fixed Docker image to use the nexus artifacts
    instead of building from source. Added proxy support.
    Updated the Docker image version for Casablanca.
    
    Change-Id: I42de44b5c09992822696b1e12344ac6fde51373d
    Issue-ID: OPTFRA-301
    Signed-off-by: Dileep Ranganathan <dileep.ranganathan@intel.com>

--- build-dockers.sh
+++ build-dockers.sh
@@ -1,36 +1,61 @@
 #!/bin/bash
+
+# The script starts in the root folder of the repo, which has the following outline
+# We fetch the version information from version.properties, build docker files and
+# do a docker push. Since the job will be run under Jenkins, it will have the Nexus
+# credentials
+
+set -e
+
 BUILD_ARGS="--no-cache"
 ORG="onap"
-VERSION="1.1.1"
-STAGING="1.1.1-STAGING"
 PROJECT="optf-has"
 DOCKER_REPOSITORY="nexus3.onap.org:10003"
 IMAGE_NAME="${DOCKER_REPOSITORY}/${ORG}/${PROJECT}"
-TIMESTAMP=$(date +"%Y%m%dT%H%M%S")
- 
+
+# Version properties
+source version.properties
+VERSION=$release_version
+SNAPSHOT=$snapshot_version
+STAGING=${release_version}-STAGING
+TIMESTAMP=$(date +"%Y%m%dT%H%M%S")Z
+
 if [ $HTTP_PROXY ]; then
-BUILD_ARGS+=" --build-arg HTTP_PROXY=${HTTP_PROXY}"
+    BUILD_ARGS+=" --build-arg HTTP_PROXY=${HTTP_PROXY}"
 fi
 if [ $HTTPS_PROXY ]; then
     BUILD_ARGS+=" --build-arg HTTPS_PROXY=${HTTPS_PROXY}"
 fi
- 
-function build_image(){
-     echo Building Image 
-     docker build -t ${IMAGE_NAME}:${VERSION} -t ${IMAGE_NAME}:latest -t ${IMAGE_NAME}:${STAGING}  conductor/docker
-     echo ... Built
+
+
+function log_ts() {  # Log message with timestamp
+    echo [DEBUG LOG at $(date -u +%Y%m%d:%H%M%S)] "$@"
+}
+
+function build_image() {
+    log_ts Building Image in folder: $PWD with build arguments ${BUILD_ARGS}
+    docker build ${BUILD_ARGS} -t ${IMAGE_NAME}:latest conductor/docker
+    log_ts ... Built
+}
+
+function tag_image() {
+    log_ts Tagging images: ${IMAGE_NAME}:\{$SNAPSHOT-${TIMESTAMP},$STAGING-${TIMESTAMP},latest\}
+    docker tag ${IMAGE_NAME}:latest ${IMAGE_NAME}:${SNAPSHOT}-${TIMESTAMP}
+    docker tag ${IMAGE_NAME}:latest ${IMAGE_NAME}:${STAGING}-${TIMESTAMP}
+    docker tag ${IMAGE_NAME}:latest ${IMAGE_NAME}:latest
+    log_ts ... Tagged images
 }
 
 function push_image(){
-     echo Pushing image starts.
-     build_image 	     
-
-     docker push ${IMAGE_NAME}:${VERSION}
-     docker push ${IMAGE_NAME}:latest
-     docker push ${IMAGE_NAME}:STAGING
-
-     echo ... Pushed $1
+    log_ts Pushing images: ${IMAGE_NAME}:\{$SNAPSHOT-${TIMESTAMP},$STAGING-${TIMESTAMP},latest\}
+    docker push ${IMAGE_NAME}:${SNAPSHOT}-${TIMESTAMP}
+    docker push ${IMAGE_NAME}:${STAGING}-${TIMESTAMP}
+    docker push ${IMAGE_NAME}:latest
+    log_ts ... Pushed images
 }
 
-push_image 
- 
+(
+    build_image
+    tag_image
+    push_image
+)
