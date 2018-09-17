acdbbe78499ca1b8f141aa5e14688e483858fbc8
Frank Sandoval <frank.sandoval@oamtechnologies.com>
2018-09-17 15:50:04 -0600

    add AAF integration and AAF simulator
    Patch #4 address comments
    Patch (#3) addresses commentse
    this patch fixes unit tests. Original commit added AAF permissions checks for HAS API
    
    Issue-ID: OPTFRA-331
    
    Change-Id: I69519beee31f57e4ac5188604d21805266a06074
    Signed-off-by: Frank Sandoval <frank.sandoval@oamtechnologies.com>

--- conductor/conductor/tests/unit/api/controller/v1/test_plans.py
+++ conductor/conductor/tests/unit/api/controller/v1/test_plans.py
@@ -35,21 +35,24 @@ class TestPlansController(base_api.BaseApiTest):
         self.assertEqual(204, actual_response.status_int)
         self.assertEqual("GET,POST", actual_response.headers['Allow'])
 
+    @mock.patch('conductor.api.adapters.aaf.aaf_authentication.authenticate')
     @mock.patch.object(plans.LOG, 'error')
     @mock.patch.object(plans.LOG, 'debug')
     @mock.patch.object(plans.LOG, 'warning')
     @mock.patch.object(plans.LOG, 'info')
     @mock.patch('conductor.common.music.messaging.component.RPCClient.call')
     def test_index_get(self, rpc_mock, info_mock, warning_mock, debug_mock,
-                       error_mock):
+                       error_mock, aaf_mock):
         req_json_file = './conductor/tests/unit/api/controller/v1/plans.json'
         params = json.loads(open(req_json_file).read())
         plan_id = str(uuid.uuid4())
         params['id'] = plan_id
         rpc_mock.return_value = {'plans': [params]}
+        aaf_mock.return_value = True
         actual_response = self.app.get('/v1/plans', extra_environ=self.extra_environment)
         self.assertEqual(200, actual_response.status_int)
 
+    @mock.patch('conductor.api.adapters.aaf.aaf_authentication.authenticate')
     @mock.patch.object(plans.LOG, 'error')
     @mock.patch.object(plans.LOG, 'debug')
     @mock.patch.object(plans.LOG, 'warning')
@@ -57,14 +60,17 @@ class TestPlansController(base_api.BaseApiTest):
     @mock.patch('conductor.common.music.messaging.component.RPCClient.call')
     def test_index_post_error(self, rpc_mock, info_mock, warning_mock,
                               debug_mock,
-                              error_mock):
+                              error_mock,
+                                aaf_mock):
         req_json_file = './conductor/tests/unit/api/controller/v1/plans.json'
         params = jsonutils.dumps(json.loads(open(req_json_file).read()))
         rpc_mock.return_value = {}
+        aaf_mock.return_value = True
         response = self.app.post('/v1/plans', params=params,
                                  expect_errors=True, extra_environ=self.extra_environment)
         self.assertEqual(500, response.status_int)
 
+    @mock.patch('conductor.api.adapters.aaf.aaf_authentication.authenticate')
     @mock.patch.object(plans.LOG, 'error')
     @mock.patch.object(plans.LOG, 'debug')
     @mock.patch.object(plans.LOG, 'warning')
@@ -72,7 +78,8 @@ class TestPlansController(base_api.BaseApiTest):
     @mock.patch('conductor.common.music.messaging.component.RPCClient.call')
     def test_index_post_success(self, rpc_mock, info_mock, warning_mock,
                                 debug_mock,
-                                error_mock):
+                                error_mock,
+                                aaf_mock):
         req_json_file = './conductor/tests/unit/api/controller/v1/plans.json'
         params = json.loads(open(req_json_file).read())
         mock_params = copy.deepcopy(params)
@@ -80,6 +87,7 @@ class TestPlansController(base_api.BaseApiTest):
 
         mock_params['id'] = plan_id
         rpc_mock.return_value = {'plan': mock_params}
+        aaf_mock.return_value = True
         params = json.dumps(params)
         response = self.app.post('/v1/plans', params=params,
                                  expect_errors=True, extra_environ=self.extra_environment)
@@ -94,26 +102,30 @@ class TestPlansController(base_api.BaseApiTest):
 
 class TestPlansItemController(base_api.BaseApiTest):
 
+    @mock.patch('conductor.api.adapters.aaf.aaf_authentication.authenticate')
     @mock.patch('conductor.common.music.messaging.component.RPCClient.call')
-    def test_index_options(self, rpc_mock):
+    def test_index_options(self, rpc_mock, aaf_mock):
         req_json_file = './conductor/tests/unit/api/controller/v1/plans.json'
         params = json.loads(open(req_json_file).read())
         plan_id = str(uuid.uuid4())
         params['id'] = plan_id
         rpc_mock.return_value = {'plans': [params]}
+        aaf_mock.return_value = True
         url = '/v1/plans/' + plan_id
         print(url)
         actual_response = self.app.options(url=url, expect_errors=True, extra_environ=self.extra_environment)
         self.assertEqual(204, actual_response.status_int)
         self.assertEqual("GET,DELETE", actual_response.headers['Allow'])
 
+    @mock.patch('conductor.api.adapters.aaf.aaf_authentication.authenticate')
     @mock.patch('conductor.common.music.messaging.component.RPCClient.call')
-    def test_index_httpmethod_notallowed(self, rpc_mock):
+    def test_index_httpmethod_notallowed(self, rpc_mock, aaf_mock):
         req_json_file = './conductor/tests/unit/api/controller/v1/plans.json'
         params = json.loads(open(req_json_file).read())
         plan_id = str(uuid.uuid4())
         params['id'] = plan_id
         rpc_mock.return_value = {'plans': [params]}
+        aaf_mock.return_value = True
         url = '/v1/plans/' + plan_id
         actual_response = self.app.put(url=url, expect_errors=True, extra_environ=self.extra_environment)
         self.assertEqual(405, actual_response.status_int)
@@ -122,35 +134,41 @@ class TestPlansItemController(base_api.BaseApiTest):
         actual_response = self.app.post(url=url, expect_errors=True, extra_environ=self.extra_environment)
         self.assertEqual(405, actual_response.status_int)
 
+    @mock.patch('conductor.api.adapters.aaf.aaf_authentication.authenticate')
     @mock.patch('conductor.common.music.messaging.component.RPCClient.call')
-    def test_index_get(self, rpc_mock):
+    def test_index_get(self, rpc_mock, aaf_mock):
         req_json_file = './conductor/tests/unit/api/controller/v1/plans.json'
         params = json.loads(open(req_json_file).read())
         plan_id = str(uuid.uuid4())
         params['id'] = plan_id
         expected_response = {'plans': [params]}
         rpc_mock.return_value = {'plans': [params]}
+        aaf_mock.return_value = True
         url = '/v1/plans/' + plan_id
         actual_response = self.app.get(url=url, expect_errors=True, extra_environ=self.extra_environment)
         self.assertEqual(200, actual_response.status_int)
         self.assertJsonEqual(expected_response,
                              json.loads(actual_response.body))
 
+    @mock.patch('conductor.api.adapters.aaf.aaf_authentication.authenticate')
     @mock.patch('conductor.common.music.messaging.component.RPCClient.call')
-    def test_index_get_non_exist(self, rpc_mock):
+    def test_index_get_non_exist(self, rpc_mock, aaf_mock):
         rpc_mock.return_value = {'plans': []}
+        aaf_mock.return_value = True
         plan_id = str(uuid.uuid4())
         url = '/v1/plans/' + plan_id
         actual_response = self.app.get(url=url, expect_errors=True, extra_environ=self.extra_environment)
         self.assertEqual(404, actual_response.status_int)
 
+    @mock.patch('conductor.api.adapters.aaf.aaf_authentication.authenticate')
     @mock.patch('conductor.common.music.messaging.component.RPCClient.call')
-    def test_index_delete(self, rpc_mock):
+    def test_index_delete(self, rpc_mock, aaf_mock):
         req_json_file = './conductor/tests/unit/api/controller/v1/plans.json'
         params = json.loads(open(req_json_file).read())
         plan_id = str(uuid.uuid4())
         params['id'] = plan_id
         rpc_mock.return_value = {'plans': [params]}
+        aaf_mock.return_value = True
         url = '/v1/plans/' + plan_id
         actual_response = self.app.delete(url=url, expect_errors=True, extra_environ=self.extra_environment)
         self.assertEqual(204, actual_response.status_int)
