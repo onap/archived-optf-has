446c640d58e5347c9caa6669f11da8942825faa2
Dileep Ranganathan <dileep.ranganathan@intel.com>
2018-10-12 05:54:32 -0700

    Enable SMS for HAS
    Load secrets from SMS in OSDF
    Helm charts uses a Job to load secrets.
    CSIT needs to load it manually.
    
    Change-Id: I09bc96e1408d4136cc479e1e5667d36b5a1b5427
    Issue-ID: OPTFRA-343
    Signed-off-by: Dileep Ranganathan <dileep.ranganathan@intel.com>

--- conductor/conductor/service.py
+++ conductor/conductor/service.py
@@ -108,6 +108,6 @@ def prepare_service(argv=None, config_files=None):
     if argv:
         gmr.TextGuruMeditation.setup_autorun(version)
     messaging.setup()
-    # TODO(Dileep): Uncomment once Helm charts to preload secrets available
-    # sms.load_secrets()
+    # Load secrets from SMS
+    sms.load_secrets()
     return conf
