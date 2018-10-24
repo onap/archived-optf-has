d6db3da91d72a2f50b0fe05d6ccba1f224bf83b2
Frank Sandoval <frank.sandoval@oamtechnologies.com>
2018-10-24 02:15:50 -0600

    enforce AAF permissions
    Issue-ID: OPTFRA-331
    
    Change-Id: I046ddef243f73ae90ca0a28184ee0decf73069ee
    Signed-off-by: Frank Sandoval <frank.sandoval@oamtechnologies.com>

--- conductor/conductor/api/adapters/aaf/aaf_authentication.py
+++ conductor/conductor/api/adapters/aaf/aaf_authentication.py
@@ -33,29 +33,47 @@ CONF = cfg.CONF
 # TBD - read values from conductor.conf
 AAF_OPTS = [
     cfg.BoolOpt('is_aaf_enabled',
-                default=True,
+                default=False,
                 help='is_aaf_enabled.'),
     cfg.IntOpt('aaf_cache_expiry_hrs',
                default='3',
                help='aaf_cache_expiry_hrs.'),
     cfg.StrOpt('aaf_url',
-               default='http://aaf-service:8100/authz/perms/user/',
+               default='https://aaf-onap-test.osaaf.org:8100/authz/perms/user/',
                help='aaf_url.'),
+    cfg.StrOpt('aaf_username',
+               default='aaf_admin@people.osaaf.org',
+               help='aaf_username.'),
+    cfg.StrOpt('aaf_password',
+               default='demo123456!',
+               help='aaf_pasword.'),
+    cfg.StrOpt('aaf_cert_file',
+               default='AAF_RootCA.cer',
+               help='aaf_cert_file.'),
+    cfg.StrOpt('aaf_cert_key_file',
+               default='org.onap.oof.keyfile',
+               help='aaf_cert_key_file.'),
+    cfg.StrOpt('aaf_ca_bundle_file',
+               default="",
+               help='aaf_ca_bundle_file.'),
     cfg.IntOpt('aaf_retries',
                default='3',
                help='aaf_retries.'),
     cfg.IntOpt('aaf_timeout',
                default='100',
                help='aaf_timeout.'),
-    cfg.ListOpt('aaf_user_roles',
-               default=['{"type": "org.onap.oof","instance": "plans","action": "GET"}',
-                      '{"type": "org.onap.oof","instance": "plans","action": "POST"}'],
+#    cfg.ListOpt('aaf_users',
+#               default=['{"clientId": "conductor","user": "oof@oof.onap.org"}'],
+    cfg.StrOpt('aaf_conductor_user',
+               default="oof@oof.onap.org",
+               help='aaf_users.'),
+    cfg.ListOpt('aaf_permissions',
+               default=['{"type": "org.onap.oof.access","instance": "*","action": "*"}'],
                help='aaf_user_roles.')
 ]
 
-CONF.register_opts(AAF_OPTS, group='aaf_authentication')
 
-AUTHZ_PERMS_USER = '{}/authz/perms/user/{}'
+CONF.register_opts(AAF_OPTS, group='aaf_authentication')
 
 EXPIRE_TIME = 'expire_time'
 
@@ -64,10 +82,35 @@ perm_cache = {}
 def clear_cache():
     perm_cache.clear()
 
+"""
+TBD: In authenticate() would prefer to walk a list of clients to map username:password to AAF user identify, but syntax
+of config.ListOpt is unclear in configuration file.  Only tested client is conductor so just match
+that for now
+    # check username, password of OOF client. There may be multiple clients, each mapped to 
+    # a user in AAF
+    aafUser = None
+    username = password = None
+    aaf_users = CONF.aaf_authentication.aaf_users
+    for users in aaf_users:
+        userDict = json.loads(users)
+        clientId = userDict["clientId"].encode('UTF-8')
+        if clientId == "conductor":
+            username = CONF.conductor_api.username
+            password = CONF.conductor_api.password
+        user = userDict["user"].encode('UTF-8')
+        if username == uid and password == passwd:
+            aafUser = user
+"""
 
 def authenticate(uid, passwd):
+    username = CONF.conductor_api.username
+    password = CONF.conductor_api.password
+    aaf_users = CONF.aaf_authentication.aaf_users
+    if username == uid and password == passwd:
+        aafUser = CONF.aaf_authentication.aaf_conductor_user
+    
     try:
-        perms = get_aaf_permissions(uid, passwd)
+        perms = get_aaf_permissions(aafUser)
         return has_valid_role(perms)
     except Exception as exp:
         LOG.error("Error Authenticating the user {} : {}: ".format(uid, exp))
@@ -80,13 +123,13 @@ return True if the user has valid permissions
 else return false
 """
 
-def has_valid_role(perms):
-    aaf_user_roles = CONF.aaf_authentication.aaf_user_roles
+def has_valid_role(userPerms):
+    aaf_permissions = CONF.aaf_authentication.aaf_permissions
 
-    permObj = json.loads(perms)
+    permObj = json.loads(userPerms)
     permList = permObj["perm"]
-    for user_role in aaf_user_roles:
-        role = json.loads(user_role)
+    for aaf_perm in aaf_permissions:
+        role = json.loads(aaf_perm)
         userType = role["type"]
         userInstance = role["instance"]
         userAction = role["action"]
@@ -104,35 +147,45 @@ Make the remote aaf api call if user is not in the cache.
 
 Return the perms
 """
-def get_aaf_permissions(uid, passwd):
-    key = base64.b64encode("{}_{}".format(uid, passwd), "ascii")
+def get_aaf_permissions(aafUser):
+    key = base64.b64encode("{}".format(aafUser), "ascii")
     time_delta = timedelta(hours = CONF.aaf_authentication.aaf_cache_expiry_hrs)
 
-# TBD - test cache logic
     perms = perm_cache.get(key)
 
     if perms and datetime.now() < perms.get(EXPIRE_TIME):
         LOG.debug("Returning cached value")
         return perms['roles']
     LOG.debug("Invoking AAF authentication API")
-    response = remote_api(passwd, uid)
+    response = remote_api(aafUser)
     perms = {EXPIRE_TIME: datetime.now() + time_delta, 'roles': response}
     perm_cache[key] = perms
     return response
 
-def remote_api(passwd, uid):
-    server_url = CONF.aaf_authentication.aaf_url.rstrip('/')
+
+"""
+The remote api is the AAF service
+
+"""
+def remote_api(aafUser):
+#    server_url = CONF.aaf_authentication.aaf_url.rstrip('/')
+    server_url = CONF.aaf_authentication.aaf_url+aafUser
     kwargs = {
         "server_url": server_url,
         "retries": CONF.aaf_authentication.aaf_retries,
-        "username": uid,
-        "password": passwd,
+        "username": CONF.aaf_authentication.aaf_username,
+        "password": CONF.aaf_authentication.aaf_password,
         "log_debug": LOG.debug,
         "read_timeout": CONF.aaf_authentication.aaf_timeout,
+        # TBD - get certs to work
+#        "cert_file": CONF.aaf_authentication.aaf_cert_file,
+#        "cert_key_file": CONF.aaf_authentication.aaf_cert_key_file,
+        # This will set TLS verify to False, see rest.py
+        "ca_bundle_file": CONF.aaf_authentication.aaf_ca_bundle_file,
     }
     restReq = rest.REST(**kwargs)
 
-    headers = {"Accept": "application/json"}
+    headers = {"Accept": "application/Perms+json;q=1.0;charset=utf-8;version=2.1,application/json;q=1.0;version=2.1,*/*;q=1.0"}
     rkwargs = {
         "method": 'GET',
         "path": '',
