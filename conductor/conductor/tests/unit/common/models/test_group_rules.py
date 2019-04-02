0f5b123847da0e62fd629b18a4ebbd791adc6509
Sudarshan Kumar <sudars19@in.ibm.com>
2019-04-03 00:28:50 +0530

    Adding Pyunit to increase HAS code coverage
    Adding Pyunit to increase HAS code coverage
    
    Issue-ID: OPTFRA-302
    Change-Id: I83eda99276ef960c88e0d51f59c04ab2104fcf19
    Signed-off-by: Sudarshan Kumar <sudars19@in.ibm.com>

---
+++ conductor/conductor/tests/unit/common/models/test_group_rules.py
@@ -0,0 +1,41 @@
+#
+# -------------------------------------------------------------------------
+#   Copyright (C) 2019 IBM.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+# -------------------------------------------------------------------------
+#
+
+"""Test class for model group_rules"""
+
+import unittest
+from conductor.common.models.group_rules import GroupRules
+
+class TestGroupRules(unittest.TestCase):
+
+    def setUp(self):
+        self.groupRules = GroupRules()
+        self.values = {'group': None, 'id': None}
+
+    def test_GroupRules(self):
+        self.assertEqual(True, self.groupRules.atomic())
+        self.assertEqual("id", self.groupRules.pk_name())
+        self.assertEqual(None, self.groupRules.pk_value())
+        self.assertEqual(self.values, self.groupRules.values())
+        self.assertEqual(None, self.groupRules.__json__().get('rule'))
+        self.assertEqual("text", self.groupRules.schema().get('id'))
+
+
+if __name__ == '__main__':
+        unittest.main()
---
+++ conductor/conductor/tests/unit/common/models/test_triage_tool.py
@@ -0,0 +1,41 @@
+#
+# -------------------------------------------------------------------------
+#   Copyright (C) 2019 IBM.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+# -------------------------------------------------------------------------
+#
+
+"""Test class for model triage_tool"""
+
+import unittest
+from conductor.common.models.triage_tool import TriageTool
+
+class TestTriageTool(unittest.TestCase):
+
+    def setUp(self):
+        self.triageTool = TriageTool()
+        self.values = {'triage_solver': 'null', 'optimization_type': None, 'triage_translator': 'null', 'id': None, 'name': None}
+
+    def test_TriageTool(self):
+        self.assertEqual(True, self.triageTool.atomic())
+        self.assertEqual("id", self.triageTool.pk_name())
+        self.assertEqual(None, self.triageTool.pk_value())
+        self.assertEqual(self.values, self.triageTool.values())
+        self.assertEqual(None, self.triageTool.__json__().get('name'))
+        self.assertEqual("text", self.triageTool.schema().get('optimization_type'))
+
+
+if __name__ == '__main__':
+        unittest.main()
