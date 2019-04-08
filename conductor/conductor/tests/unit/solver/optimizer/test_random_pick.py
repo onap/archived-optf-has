cf310cca2f3d408497a2476eb2b7afae1c5ad94e
Sudarshan Kumar <sudars19@in.ibm.com>
2019-04-08 17:57:48 +0530

    Pyunit for random_pick.py
    Pyunit for random_pick.py
    
    Issue-ID: OPTFRA-302
    Change-Id: I69b7443af188317978bc06194412f4689267c56a
    Signed-off-by: Sudarshan Kumar <sudars19@in.ibm.com>

---
+++ conductor/conductor/tests/unit/solver/optimizer/test_random_pick.py
@@ -0,0 +1,42 @@
+#
+# -------------------------------------------------------------------------
+#   Copyright (C) 2019 IBM.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+# -------------------------------------------------------------------------
+#
+
+"""Test class for optimizer random_pick.py"""
+
+import unittest
+from conductor.common.music import api
+from conductor.solver.optimizer.random_pick import RandomPick
+from conductor.solver.request import demand
+from oslo_config import cfg
+
+
+class TestRandomPick(unittest.TestCase):
+
+    def setUp(self):
+        self.music = api.API()
+        self.conf = cfg.CONF
+        self.randomPick = RandomPick(self.conf)
+
+    def test_search(self):
+        _demand_list = list()
+        self.assertEqual(None, self.randomPick.search(_demand_list, '_request').current_demand)
+
+
+if __name__ == '__main__':
+    unittest.main()
