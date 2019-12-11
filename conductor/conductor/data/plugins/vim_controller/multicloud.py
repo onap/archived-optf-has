9f8f7046d3978b6dbd05c7e5ad645b3dd9201249
dhebeha <dhebeha.mj71@wipro.com>
2019-12-11 12:35:37 +0530

    Add AAF root cert location to conductor.conf
    Modify multicloud.py to verify the session
    using AAF root certificate if https
    
    Issue-ID: OPTFRA-328
    Change-Id: Ica47cbda3821120a020f34c97a4e398e2f7bbda5
    Signed-off-by: dhebeha <dhebeha.mj71@wipro.com>

--- conductor/conductor/data/plugins/vim_controller/multicloud.py
+++ conductor/conductor/data/plugins/vim_controller/multicloud.py
@@ -45,6 +45,12 @@ MULTICLOUD_OPTS = [
     cfg.StrOpt('server_url_version',
                default='v0',
                help='The version of Multicloud API.'),
+    cfg.StrOpt('certificate_authority_bundle_file',
+               default='certificate_authority_bundle.pem',
+               help='Certificate Authority Bundle file in pem format. '
+                    'Must contain the appropriate trust chain for the '
+                    'Certificate file.'),
+    cfg.BoolOpt('enable_https_mode', default = False, help='enable HTTPs mode for multicloud connection'),
 ]
 
 CONF.register_opts(MULTICLOUD_OPTS, group='multicloud')
@@ -109,6 +115,9 @@ class MULTICLOUD(base.VimControllerBase):
             "read_timeout": self.timeout,
         }
         self.rest = rest.REST(**kwargs)
+        if(self.conf.multicloud.enable_https_mode):
+            self.rest.server_url = self.base[:4]+'s'+self.base[4:]
+            self.rest.session.verify =self.conf.multicloud.certificate_authority_bundle_file    
 
     def check_vim_capacity(self, vim_request):
         LOG.debug("Invoking check_vim_capacity api")
