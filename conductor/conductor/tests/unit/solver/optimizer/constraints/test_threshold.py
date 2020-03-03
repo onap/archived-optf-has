ebb4d728c3e74cc12721d4c8a711db9586f2be0d
krishnaa96 <krishna.moorthy6@wipro.com>
2020-03-03 15:01:51 +0530

    Add functionalities to support NSSI selection
    Add threshold constraint
    Modify AAI plugin to get NSSIs from AAI
    
    Issue-ID: OPTFRA-677
    Signed-off-by: krishnaa96 <krishna.moorthy6@wipro.com>
    Change-Id: Ic9bc97aca3835eba99d0a3f1580c281c3856b1cf

---
+++ conductor/conductor/tests/unit/solver/optimizer/constraints/test_threshold.py
@@ -0,0 +1,63 @@
+#
+# -------------------------------------------------------------------------
+#   Copyright (C) 2020 Wipro Limited.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+# -------------------------------------------------------------------------
+#
+
+import json
+import unittest
+from conductor.solver.optimizer.constraints.threshold import Threshold
+from conductor.solver.optimizer.decision_path import DecisionPath
+from conductor.solver.request.demand import Demand
+
+
+class TestThreshold(unittest.TestCase):
+
+    def test_solve(self):
+
+        candidates_file = './conductor/tests/unit/data/plugins/inventory_provider/nssi_candidate.json'
+        candidates = json.loads(open(candidates_file).read())
+
+        properties = {'attribute': 'latency', 'threshold': 30, 'operator': 'lte'}
+
+        threshold_obj = Threshold('urllc_threshold', 'threshold', ['URLLC'], _priority=0, _properties=properties)
+
+        decision_path = DecisionPath()
+        decision_path.current_demand = Demand('URLLC')
+
+        self.assertEqual(candidates, threshold_obj.solve(decision_path, candidates))
+
+        properties = {'attribute': 'latency', 'threshold': 10, 'operator': 'lte'}
+
+        threshold_obj = Threshold('urllc_threshold', 'threshold', ['URLLC'], _priority=0, _properties=properties)
+
+        self.assertEqual([], threshold_obj.solve(decision_path, candidates))
+
+        properties = {'attribute': 'exp_data_rate_ul', 'threshold': 70, 'operator': 'gte'}
+
+        threshold_obj = Threshold('urllc_threshold', 'threshold', ['URLLC'], _priority=0, _properties=properties)
+
+        self.assertEqual(candidates, threshold_obj.solve(decision_path, candidates))
+
+        properties = {'attribute': 'exp_data_rate_ul', 'threshold': 120, 'operator': 'gte'}
+
+        threshold_obj = Threshold('urllc_threshold', 'threshold', ['URLLC'], _priority=0, _properties=properties)
+
+        self.assertEqual([], threshold_obj.solve(decision_path, candidates))
+
+
+if __name__ == "__main__":
+    unittest.main()
