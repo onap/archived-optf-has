9fe20d96149fceaccab33673147fe9ee72ed0604
Sithara Nambiar <sitharav.aredath@huawei.com>
2020-03-20 16:14:07 +0530

    NST selection changes
    Issue-ID: OPTFRA-675
    
    Signed-off-by: Sithara Nambiar <sitharav.aredath@huawei.com>
    Change-Id: If764cac3d17af107b1473a1ce6fdfb8ae266b446

---
+++ conductor/conductor/data/plugins/file_system/config.py
@@ -0,0 +1,95 @@
+#
+# -------------------------------------------------------------------------
+#   Copyright (c) 2015-2017 AT&T Intellectual Property
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+# -------------------------------------------------------------------------
+#
+
+import time
+import json
+from oslo_config import cfg
+from oslo_log import log
+import os
+import sys
+from conductor.data.plugins.file_system import base
+from conductor.data.plugins.triage_translator.triage_translator import TraigeTranslator
+from conductor.i18n import _LE
+
+LOG = log.getLogger(__name__)
+
+CONF = cfg.CONF
+
+CONFIG_OPTS = [
+    cfg.IntOpt('cache_refresh_interval',
+               default=1440,
+               help='Interval with which to refresh the local cache, '
+                    'in minutes.'),
+]
+
+CONF.register_opts(CONFIG_OPTS, group='config')
+
+
+class CONFIG(base.FileSystemBase):
+    """SDN Service Controller"""
+
+    def __init__(self):
+        """Initializer"""
+
+        # FIXME(jdandrea): Pass this in to init.
+        self.conf = CONF
+        self.file_path = ""
+        self.triage_translator=TraigeTranslator()
+
+    def initialize(self):
+        """Perform any late initialization."""
+        # self.filter_candidates([])
+        pass
+
+    def get_candidates(self, demands, plan_info, triage_translator_data):
+        self.triage_translator.getPlanIdNAme(plan_info['plan_name'], plan_info['plan_id'],triage_translator_data)
+        resolved_demands = {}
+        with open(os.path.join(sys.path[0],'conductor/conductor/data/plugins/file_system/NST.json'), 'r') as openfile:
+            nst_object = json.load(openfile)
+            for name, requirements in demands.items():
+                self.triage_translator.addDemandsTriageTranslator(name, triage_translator_data)
+                resolved_demands[name] = []
+                for requirement in requirements:
+                    inventory_type = requirement.get('inventory_type').lower()
+                    if inventory_type == 'nst':#nst
+                        if not nst_object or len(nst_object) < 1:
+                            LOG.debug("candidtaes are B not "
+                                  "available ")
+                        else:
+                            for region in nst_object['NST']:
+
+                                candidate = dict()
+                                candidate['inventory_provider'] = 'file_system'
+                                candidate['inventory_type'] = 'NST'
+                                candidate["candidate_id"] = region["NST_name"]
+                                candidate['cost'] = 2
+                                for key in region:
+                                    candidate[key] = region[key]
+                                resolved_demands[name].append(candidate)
+                                LOG.debug(">>>>>>> Candidate <<<<<<<")
+                                LOG.debug(json.dumps(candidate, indent=4))
+            return resolved_demands
+
+
+
+
+
+    def name(self):
+        """Return human-readable name."""
+        return "file_system"
---
+++ conductor/conductor/tests/unit/data/plugins/inventory_provider/test_file_system.py
@@ -0,0 +1,34 @@
+import unittest
+
+import conductor.data.plugins.file_system.config as config
+from oslo_config import cfg
+
+class TestConfig(unittest.TestCase):
+
+    def setUp(self):
+        cli_opts = [
+            cfg.BoolOpt('debug',
+                        short='d',
+                        default=False,
+                        help='Print debugging output.'),
+        ]
+        cfg.CONF.register_cli_opts(cli_opts)
+        self.config_ep = config.CONFIG()
+
+    def test_get_candidates(self):
+        plan_info = {
+            'plan_name': 'nst_selection',
+            'plan_id': 'id'
+        }
+        triage_translator_data = {'plan_id': None, 'plan_name': [None]}
+        demands = {'NST': [{'inventory_provider': 'file_system', 'inventory_type': 'NST'}]}
+        actual_result = self.config_ep.get_candidates(demands, plan_info=plan_info,
+                                                      triage_translator_data=triage_translator_data)
+        expected_result ={'NST': [{'candidate_id': 'NST1', 'NST_name': 'NST1', 'cost': 2, 'inventory_provider': 'file_system', 'inventory_type': 'NST', 'latency': 10, 'reliability': 100}, {'candidate_id': 'NST2', 'NST_name': 'NST2', 'cost': 2, 'inventory_provider': 'file_system', 'inventory_type': 'NST', 'latency': 1, 'reliability': 90}]}
+        self.assertEqual(actual_result, expected_result)
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
