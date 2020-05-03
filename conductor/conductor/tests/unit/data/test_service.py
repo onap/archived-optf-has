0010e31e1514edc9fc15f0d2cdb11c9d76e627c6
Sithara Nambiar <sitharav.aredath@huawei.com>
2020-05-03 19:54:34 +0530

    NST selection changes has
    Issue-ID: OPTFRA-675
    
    Signed-off-by: Sithara Nambiar <sitharav.aredath@huawei.com>
    Change-Id: I652a2cb1c6d14bd87d818d6ce6d05dceae3abc31

--- conductor/conductor/tests/unit/data/test_service.py
+++ conductor/conductor/tests/unit/data/test_service.py
@@ -28,6 +28,7 @@ import yaml
 from conductor.common.utils import conductor_logging_util as log_util
 from conductor.data.plugins.inventory_provider import extensions as ip_ext
 from conductor.data.plugins.service_controller import extensions as sc_ext
+from conductor.data.plugins.file_system import extensions as fs_ext
 from conductor.data.plugins.vim_controller import extensions as vc_ext
 from conductor.data.service import DataEndpoint
 from oslo_config import cfg
@@ -42,10 +43,13 @@ class TestDataEndpoint(unittest.TestCase):
             vc_ext.Manager(cfg.CONF, 'conductor.vim_controller.plugin'))
         sc_ext_manager = (
             sc_ext.Manager(cfg.CONF, 'conductor.service_controller.plugin'))
+        fs_ext_manager = (
+            fs_ext.Manager(cfg.CONF, 'conductor.file_system.plugin'))
 
         self.data_ep = DataEndpoint(ip_ext_manager,
                                     vc_ext_manager,
-                                    sc_ext_manager)
+                                    sc_ext_manager,
+                                    fs_ext_manager)
 
     def tearDown(self):
         pass
