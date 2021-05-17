6cb13a67a0d06b3df037557144ae49f93388225e
Malarvizhi Paramasivam <malarvizhi.44@wipro.com>
2021-05-17 16:53:59 +0530

    Fix sonar issues
    Issue-ID: OPTFRA-946
    Signed-off-by: Malarvizhi Paramasivam <malarvizhi.44@wipro.com>
    Change-Id: I9caa57c88eae3b58e492dee621498caa054028ea
    Signed-off-by: Malarvizhi Paramasivam <malarvizhi.44@wipro.com>

--- conductor/conductor/solver/rest/latency_data_loader.py
+++ conductor/conductor/solver/rest/latency_data_loader.py
@@ -20,7 +20,7 @@
 import csv
 import collections
 import json
-from conductor.common.models import  region_placeholders
+from conductor.common.models import region_placeholders
 from conductor.common.music import api
 
 
@@ -31,89 +31,53 @@ class LatencyDataLoader(object):
         music = api.API()
         print("Music version %s" % music.version())
 
-
     # load data into region place holder
     def load_into_rph(self, json_data):
-         datamap = collections.OrderedDict()
-         group_map = collections.OrderedDict()
-         datamap = json.loads(json_data)
+        datamap = collections.OrderedDict()
+        group_map = collections.OrderedDict()
+        datamap = json.loads(json_data)
 
+        # for i, j in enumerate(datamap):
+        #    group_map[j['group']] = j['countries']
 
-         #for i, j in enumerate(datamap):
-         #    group_map[j['group']] = j['countries']
+        music = api.API()
 
-         music = api.API()
+        # for row in group_map:
+        # music.row_create()
 
-         #for row in group_map:
-         #   music.row_create()
+        kwargs = {'keyspace': 'conductor_inam', 'table': 'region_placeholders', 'pk_name': 'id'}
+        for row in enumerate(datamap):
+            kwargs['pk_value'] = id(row)
+            kwargs['values'] = {'region_name': row['group'], 'countries': row['countries']}
+            music.row_create(**kwargs)
 
-         kwargs = {'keyspace': 'conductor_inam', 'table': 'region_placeholders', 'pk_name': 'id'}
-         for row in enumerate(datamap):
-             kwargs['pk_value'] = id()
-             kwargs['values'] = {'region_name': row['group'], 'countries': row['countries']}
-             music.row_create(**kwargs)
-
-
-
-         print(group_map)
-
+        print(group_map)
 
     def load_into_country_letancy(self, json_data):
-         datamap = collections.OrderedDict()
-         group_map = collections.OrderedDict()
-         datamap = json.loads(json_data)
+        datamap = collections.OrderedDict()
+        group_map = collections.OrderedDict()
+        datamap = json.loads(json_data)
 
+        # for i, j in enumerate(datamap):
+        #    group_map[j['group']] = j['countries']
 
-         #for i, j in enumerate(datamap):
-         #    group_map[j['group']] = j['countries']
+        music = api.API()
 
-         music = api.API()
+        # for row in group_map:
+        #   music.row_create()
 
-         #for row in group_map:
-         #   music.row_create()
+        kwargs = {'keyspace': 'conductor_inam', 'table': 'country_latency', 'pk_name': 'id'}
+        for row in enumerate(datamap):
+            kwargs['pk_value'] = id(row)
+            kwargs['values'] = {'country_name': row['country_name'], 'groups': row['groups']}
+            music.row_create(**kwargs)
 
-         kwargs = {'keyspace': 'conductor_inam', 'table': 'country_latency', 'pk_name': 'id'}
-         for row in enumerate(datamap):
-             kwargs['pk_value'] = id()
-             kwargs['values'] = {'country_name': row['country_name'], 'groups': row['groups']}
-             music.row_create(**kwargs)
+        print(group_map)
 
-
-
-         print(group_map)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#json_string = '[{"group": "EMEA-CORE1",  "countries" : "FRA|DEU|NLD|GBR1"},' \
+# json_string = '[{"group": "EMEA-CORE1",  "countries" : "FRA|DEU|NLD|GBR1"},' \
 #              '{"group": "EMEA-CORE2",  "countries" : "FRA|DEU|NLD|GBR2"},' \
 #              '{"group": "EMEA-CORE3",  "countries" : "FRA|DEU|NLD|GBR3"},' \
 #              '{"group": "EMEA-CORE4",  "countries" : "FRA|DEU|NLD|GBR4"}]'
 
-#test = LatencyDataLoader()
-#test.parseJSON(json_string)
-
-
-
+# test = LatencyDataLoader()
+# test.parseJSON(json_string)
